a0b1331e8f74b5f760c4359cde1814ebcd1f9d1f
luxinjian <luxinjian.cn@gmail.com>
2015-07-09 15:10:33 +0800

    Fix favicon 404.

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.use(cookieParser());
 app.use(morgan(':method :url :status'));
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    switch(req.path) {
+        case '/favicon.ico':
+            res.writeHead(200, {'Content-Type': 'image/x-icon'});
+            break;
+        default:
+            next();
+    }
+});
+
 app.set('port', process.env.PORT || 9334);
 app.listen(app.get('port'), function() {
     console.log('Server listening on port ' + app.get('port'));
