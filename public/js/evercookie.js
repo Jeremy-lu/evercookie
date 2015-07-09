c1627a32435be27c3c68651bed94a42b94269ca6
luxinjian <luxinjian.cn@gmail.com>
2015-07-09 14:50:51 +0800

    Add evercookie-cookie.

--- public/js/evercookie.js
+++ public/js/evercookie.js
@@ -1,10 +1,35 @@
 var evercookie = {};
 
 evercookie.get = function(key, callback) {
-    var result = 'result list: ';
-    callback(result);
+    var resultArr = [];
+    resultArr.push('cookie  --->   ' + ecCookie(key));
+    callback(resultArr);
 };
 
-evercookie.set = function(key) {
-    return true;
+evercookie.set = function(key, val) {
+    ecCookie(key, val);
 };
+
+function ecCookie(key, val) {
+    if (val) {
+        var d = new Date();
+        d.setFullYear(d.getFullYear() + 10);
+        document.cookie = key + '=' + val + '; expires=' + d.toUTCString();
+    } else {
+        return getValueFromStr(document.cookie, key);
+    }
+}
+
+function getValueFromStr(str, key, splitChar) {
+    splitChar = splitChar || ';';
+    var cookieArray = str.split(splitChar);
+    for (var i = 0, il = cookieArray.length; i < il; i++) {
+        var cookie = cookieArray[i];
+        var splitIndex = cookie.indexOf('=');
+        var tmpKey = cookie.slice(0, splitIndex).trim();
+        var val = cookie.slice(splitIndex + 1).trim();
+
+        if (tmpKey === key) return val;
+    }
+    return null;
+}
