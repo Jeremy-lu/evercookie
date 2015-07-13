e918ec0c5359423a73f655952bfd10057a5f87d6
luxinjian <luxinjian.cn@gmail.com>
2015-07-13 11:23:18 +0800

    Add trim() function.

--- public/js/evercookie.js
+++ public/js/evercookie.js
@@ -1,3 +1,10 @@
+// Add trim function
+if (!String.prototype.trim) {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+    };
+}
+
 var evercookie = {};
 
 evercookie.get = function(key, callback) {
